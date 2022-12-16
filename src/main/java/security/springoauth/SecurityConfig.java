8a9a316fc100c8d50e15f524f075f1c75a414faa
juhyeon <juhyeon1114@gmail.com>
2022-12-16 09:50:48 +0900

    Http basic

--- src/main/java/security/springoauth/SecurityConfig.java
+++ src/main/java/security/springoauth/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,31 +30,47 @@ import java.io.IOException;
 //
 //}
 
+/**
+ *
+ */
+//@Configuration
+//@Slf4j
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Throwable {
+//        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+//
+//        /**
+//         * 인증 방식 2가지
+//         */
+//        httpSecurity.formLogin(); // #1
+////        httpSecurity.httpBasic(); // #2
+//
+//        /**
+//         * 인증에 실패했을 때 처리
+//         */
+//        httpSecurity.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+//            @Override
+//            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//                log.info("=== Custom entry point");
+//            }
+//        });
+//        return httpSecurity.build();
+//    }
+//}
+
+/**
+ * Http Basic 챕터
+ */
 @Configuration
 @Slf4j
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Throwable {
         httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
-
-        /**
-         * 인증 방식 2가지
-         */
-        httpSecurity.formLogin(); // #1
-//        httpSecurity.httpBasic(); // #2
-
-        /**
-         * 인증에 실패했을 때 처리
-         */
-        httpSecurity.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
-            @Override
-            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-                log.info("=== Custom entry point");
-            }
-        });
-
+//        httpSecurity.httpBasic();
+        httpSecurity.httpBasic().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+//        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return httpSecurity.build();
     }
-
 }
